057353949723db8dbc654f1c3bca0ef60766c642
Julio Bermudez <jbermudezfc90@gmail.com>
2020-06-21 11:40:06 -0400

    changed payload limiy

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ passport.use(
 app.use(cors());
 app.use(passport.initialize());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({ extended: false, limit: '5mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
