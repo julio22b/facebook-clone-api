49f5273f74c1d76f5e2062f74890716507450016
Julio Bermudez <jbermudezfc90@gmail.com>
2020-06-30 18:02:08 -0400

    remove * route

--- app.js
+++ app.js
@@ -108,10 +108,6 @@ app.use(express.urlencoded({ extended: false, limit: '5mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.use('/posts', indexRouter);
 app.use('/users', usersRouter);
 
